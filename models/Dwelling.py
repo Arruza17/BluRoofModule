57fb56e76e46d0465d12a48f2061f9d540650a6b
Arruza17 <ander.arruza@gmail.com>
2021-12-15 13:20:15 +0100

    Dwelling & Comment done in python

---
+++ models/Dwelling.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#enum = selection
+#first commit
+from odoo import models, fields, api
+
+class Dwelling(models.Model):
+        _name = 'bluroof.dwelling'
+
+        address = fields.String()
+        hasWiFi = fields.Boolean()
+        squareMeters = fields.Double()
+        neighbourhood_id = fields.Many2one('bluroof.neighbourhood',
+        ondelete = 'cascade', String = "Neighbourhood", index = True)
+        constructionDate = fields.Date()
+        host_id = fields.Many2one('bluroof.owner',
+        ondelete = 'cascade', String = "Host", index = True)
+        rating = fields.Float()
+        comments = fields.One2many('bluroof.comment',
+        ondelete = 'cascade', String = "Comments", index = True)
+        
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
