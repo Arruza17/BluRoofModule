7d2876e9160955bc7f8fa92ccb7284f7e99446cc
yeraysampedro <83076671+yeraysampedro@users.noreply.github.com>
2022-01-05 21:53:58 +0100

    Visual Modifications
    Manifest.py modified in order to write a more suitable description as well as added an image to the module.

--- __manifest__.py
+++ __manifest__.py
@@ -3,21 +3,20 @@
     'name': "BluRoof application",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Application to make CRUD operations with Dwellings, Users and all the possible options between them""",
 
     'description': """
-        Long description of module's purpose
+        Application used to create information about Dwellings and Users. The last ones will be able to comment on dwellings. 
     """,
 
     'author': "BluRoof",
-    'website': "http://www.yourcompany.com",
+    'website': "https://github.com/Arruza17/BluRoofModule",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Real State',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -26,8 +25,10 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-    ],
+        'views/templates.xml',      
+ 	    'views/bluroof_view.xml',
+   ],
+   
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
