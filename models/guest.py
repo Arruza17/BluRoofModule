1d65184976467ad685d7eb293b13f074c9de2ecb
yeraysampedro <83076671+yeraysampedro@users.noreply.github.com>
2022-02-02 22:31:07 +0100

    Validations
    Validations working as well as search

--- models/guest.py
+++ models/guest.py
@@ -2,6 +2,7 @@
 from odoo import api
 from odoo import fields
 from odoo import models
+from odoo import exceptions
 
 class guest (models.Model):
     _inherit = 'bluroof.user'
@@ -18,7 +19,6 @@ class guest (models.Model):
     comments = fields.One2many('bluroof.comment', 'commenter_id', string="Comments")
 
     @api.constrains('actualState')
-    def _check_tittleNotNull(self):
-        for record in self:
-            if (not (record.actualState and record.actualState.strip())):
-                raise ValidationError("You must input the type of actual state")
+    def _check_actual_state_not_null(self):
+            if (not (self.actualState and self.actualState.strip())):
+                raise exceptions.ValidationError("You must input the type of actual state")
--- models/user.py
+++ models/user.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from asyncio.windows_events import NULL
 from odoo import api
 from odoo import fields
 from odoo import models
@@ -7,6 +6,7 @@ from odoo import models
 class user (models.Model):
     _inherit = 'res.users'
     _name = 'bluroof.user'
+
     birthDate = fields.Date(string="Date of birth")
     
     STATUS = [
@@ -26,12 +26,6 @@ class user (models.Model):
     lastPasswordChange = fields.Datetime(string="Last password change")
     phoneNumber = fields.Char(string="Telephone number")
 
-    @api.onchange('phoneNumber')
-    def _check_empty_phone(self):
-        if self.phoneNumber==NULL:
-            return{
-               'warning': {'title': "No mobile",'message': "Your must imput your mobile",},
-            }
 
 
     
